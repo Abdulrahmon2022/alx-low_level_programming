50891b53e6c1d689594426bc318cb7769a0baea6
Abdulrahmon2022 <abdulrahmanonifade@gmail.com>
2022-12-08 16:32:17 -0800

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry Point
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n") ;
+	return (0) ;
 }	
